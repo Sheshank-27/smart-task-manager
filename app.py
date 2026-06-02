7ef76dae45c3fb8d590dafe9b006768f3624aba7
sheshank-27 <sheshanklakhi16@gmail.com>
2026-06-02 14:24:20 +0530

    added task manager features

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
-from flask import Flask
-app=Flask(__name__)
- 
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tasks = []
+
 @app.route('/')
 def home():
-    return "Smart Task Manager Running"
+    return render_template('index.html', tasks=tasks)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000)
+@app.route('/add', methods=['POST'])
+def add_task():
+
+    task_name = request.form['task']
+    priority = request.form['priority']
+
+    task = {
+        "name": task_name,
+        "priority": priority,
+        "status": "Pending"
+    }
+
+    tasks.append(task)
+
+    return redirect('/')
+
+@app.route('/complete/<int:index>')
+def complete_task(index):
+
+    tasks[index]["status"] = "Completed"
+
+    return redirect('/')
+
+@app.route('/delete/<int:index>')
+def delete_task(index):
+
+    tasks.pop(index)
+
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
